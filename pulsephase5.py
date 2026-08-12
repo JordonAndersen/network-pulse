0ced2642014a903dacb5c330dc0db088af2af506
jordonandersen <jordonandsersen9@gmail.com>
2026-08-12 17:20:05 -0600

    created the workers to excute the task in a shorter time. currently shows all active devices on the net

--- pulsephase5.py
+++ pulsephase5.py
@@ -17,29 +17,38 @@ import concurrent.futures
 
 
 network_ips = []
+discovered_hosts = {}
+while True:
+    try:
+        for host_ips in ip_address.hosts():
+            ip_strings = str(host_ips)   
+            network_ips.append(ip_strings)
 
-for host_ips in ip_address.hosts():
-    ip_strings = str(host_ips)   
-    network_ips.append(ip_strings)
+        def ping_host(ip):
+        
+            #creating a ping command to test connectivity to the target ip address. 
+            ping_cmd = ['ping','-c','1', '-W', '1',ip]
+            #running the ping command using subprocess module and capturing the output.
+            results = subprocess.run(ping_cmd, stdout= subprocess.DEVNULL, stderr=subprocess.DEVNULL)         
+            #check the return code to see if its 0, if 0 it is successful
+            if results.returncode == 0:
+                return (ip, "UP")
+            else:
+                return (ip, "DOWN")
+            
 
-def ping_host(ip):
-     #creating time stamp
-    pulse_time = dt.datetime.now()
-    #interating over the dictionary. 
-    for ip in network_ips:   
-        #creating a ping command to test connectivity to the target ip address.
-        ping_cmd = ['ping','-c','1',ip]
-        #running the ping command using subprocess module and capturing the output.
-        results = subprocess.run(ping_cmd, stdout= subprocess.DEVNULL, stderr=subprocess.DEVNULL)         
-        #check the return code to see if its 0, if 0 it is successful
-        if results.returncode == 0:
-            print(f"{pulse_time}: Ping to {ip} successful")
-        else:
-                print(f"Ping to {ip} failed.")
+        with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+            results = executor.map(ping_host, network_ips)
 
+            
+        for found_ips, status in results:
+            if status == 'UP':
+                discovered_hosts[found_ips] = "UP"
 
-
-     
+        print(discovered_hosts)
+    except KeyboardInterrupt:
+        print(" Stopping Network Pulse...")  
+        exit()
 
 
 # while True:
