a74723cdd05698d2605e4bf2f60086a60e0795d2
jordonandersen <jordonandsersen9@gmail.com>
2026-08-20 19:58:48 -0600

    pushing updates from local

--- pulsephase5.py
+++ pulsephase5.py
@@ -51,27 +51,3 @@ while True:
         exit()
 
 
-# while True:
-#     #Try and except for ending the program 
-#     try:
-#         #creating time stamp
-#         pulse_time = dt.datetime.now()
-#         #interating over the dictionary. 
-#         for ip in network_ips:   
-#             #creating a ping command to test connectivity to the target ip address.
-#             ping_cmd = ['ping','-c','1',ip]
-
-#             #running the ping command using subprocess module and capturing the output.
-#             results = subprocess.run(ping_cmd, stdout= subprocess.DEVNULL, stderr=subprocess.DEVNULL) 
-            
-#             #check the return code to see if its 0, if 0 it is successful
-#             if results.returncode == 0:
-#                 print(f"{pulse_time}: Ping to {ip} successful")
-#             else:
-#                     print(f"Ping to {ip} failed.")
-
-#         #add sleep so it doesn't run over and over again. 
-#         time.sleep(30) 
-#     except KeyboardInterrupt:
-#         print(" Stopping Network Pulse...")   
-#         exit()   
